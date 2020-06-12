9610bb4fafe5683b9af8971ccd6d098de6f18dda
sujay.bg <sujay.bg@rubrik.com>
2020-06-12 16:45:03 +0530

    Updates

---
+++ precache-manifest.eb288144e8067f89eaabb8bd58cdf8fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd3047d09f0bdb454ee82ea6261a07f",
+    "url": "/atlantis/index.html"
+  },
+  {
+    "revision": "bcd369158a86bdba5a02",
+    "url": "/atlantis/static/css/main.b4f09642.chunk.css"
+  },
+  {
+    "revision": "b4fefa948e63ee2c6713",
+    "url": "/atlantis/static/js/2.6b32a7d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/atlantis/static/js/2.6b32a7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd369158a86bdba5a02",
+    "url": "/atlantis/static/js/main.c49dda37.chunk.js"
+  },
+  {
+    "revision": "2bf282de1cfd898bc2d5",
+    "url": "/atlantis/static/js/runtime-main.54baffd1.js"
+  }
+]);
